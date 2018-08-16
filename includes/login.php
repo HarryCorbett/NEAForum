877cdc708031054237328efc60d9a205ae977be6
Harry <harrycorbett13@hotmail.co.uk>
2018-08-16 16:49:39 +0100

    Commit

--- includes/login.php
+++ includes/login.php
@@ -6,26 +6,43 @@ $_SESSION['message'] = '';
 
 if(isset($_POST['email'])){
 
+    //Prevent sql injection
     $email = mysqli_real_escape_string($conn, $_post['email']);
     $password = mysqli_real_escape_string($conn, $_post['password']);
 
-    $sqlemail = ("select * from users where email = $email limit 1");
-    //select count(*) from users where email = '$email' and password = '$password' limit 1
 
-    $result = mysqli_query($conn, $sqlemail);
+    $sql = "select password from users where email = '$email'";
+    $result = @mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $hash = $row['password'];
 
 
-    if(mysqli_num_rows($result) === 1);{
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        if (password_verify($password, $row['password'])) {
-            echo "Login successful";
-        }else{
-            echo "details incorrect";
+    if ($hash === NULL){
+        echo 'didnt work';
+    $_session['message'] = 'This email is not linked to an account';
+    }else{
+
+        if (password_verify($password,$sql));
+        {
+            $sql = "select id from users where email = '$email' and password = '$hash'";
+            $result = @mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+            $_session['user'] = $row['user'];
+
+            header("location: index.php");
         }
 
-    }
 
+    }
 }
+
+
+
+
+
+
+
 
 
 
